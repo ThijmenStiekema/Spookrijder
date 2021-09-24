4daac6e5a8d2d869633ae883e5300645b0abc896
Mark172004 <74767453+Maku-gif@users.noreply.github.com>
2021-09-24 08:56:03 +0000

    scoreboard gemaakt

--- script.js
+++ script.js
@@ -1,49 +1,71 @@
-let road;
+var road, auto, score = 0;
+
 function preload() {
   road = loadImage('assets/road.png');
+  auto = loadImage('assets/EchteAuto.png');
 }
 
-let auto;
-function preload() {
-  auto = loadImage('assetsAuto.png');
-}
 
 function setup() {
   createCanvas(1280, 960)
-  car = new Car(10,4,100,200,5,-2);
+  car1 = new Car(1, 3, 0, 0, 0, 0);
+  player = new Player();
 }
 
-class Player{
-  constructor(Snelheid,){
+class Player {
+  constructor() {
+  }
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+     x -= 5;
+    
+  } else if (keyCode === RIGHT_ARROW) {
     
   }
 }
 
-class Car{
-  constructor(speed, lane, x, y, vx, vy){
+class Car {
+  constructor(speed, lane, x, y, vx, vy) {
     this.speed = speed;
     this.lane = lane;
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
+    this.y = 0;
+    this.x = 520;
+    this.autowidth = 
+    this.autohieght = 
   }
-  draw(){
-    rect(this.x, this.y,);
-    this.x = this.x + this.vx;
-    this.y = this.y + this.vy;
-    if(this.x <= 0 || this.x >= 1280){
-    this.vx = this.vx * -1;
+  draw() {
+    if (this.lane = 3)
+    {
+      this.size = 1;
+      image(auto, this.x, this.y);
+      this.vy = 1;
+      this.x = this.x + this.vx;
+      this.y = this.y + this.vy;
+      auto.resize(this.autowidth)
     }
-   else if(this.y <= 0 || this.y >= 960){
-   this.vy = this.vy * -1;
-    }
+    
+    //this.x = this.x + this.vx;
+    //this.y = this.y + this.vy;
+    //if (this.x <= 0 || this.x >= 1280) {
+      //this.vx = this.vx * -1;
+    //}
+    //else if (this.y <= 0 || this.y >= 960) {
+    //  this.vy = this.vy * -1;
+    //}
   }
 }
 
-function draw(){
+function draw() {
   background('blue');
   image(road, 0, 0);
-  Car.draw();
+  car1.draw();
+  textSize(20);
+  text("Score: " + score, 20, 120);
 }
 
