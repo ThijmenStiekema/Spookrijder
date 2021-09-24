dc41459758291f13b7cc71d470bd350bc821e48e
Mark172004 <74767453+Maku-gif@users.noreply.github.com>
2021-09-24 12:58:34 +0000

    player gemaakt

--- script.js
+++ script.js
@@ -5,50 +5,60 @@ function preload() {
   auto = loadImage('assets/EchteAuto.png');
 }
 
-
 function setup() {
   createCanvas(1280, 960)
   car1 = new Car(1, 3, 0, 0, 0, 0);
-  player = new Player();
+  player = new Player(100,'green', 200, 750, 5, -2);
 }
 
 class Player {
-  constructor() {
+  constructor(radius, kleur, x, y, vx, vy){
+   this.radius = radius;
+   this.kleur = kleur;
+   this.x = x;
+   this.x = 600;
+   this.y = 5;
+   this.y = y;
+   this.vx = vx;
+   this.vy = vy;
+  }
+  draw(){
+  fill('black')
+  rect(this.x,this.y,this.radius,this.radius);
+  if(this.x <= 0 || this.x >= 1280){
+  this.vx = this.vx * -1;
+  }
+  else if(this.y <= 0 || this.y >= 960){
+  this.vy = this.vy * -1;
+  }
   }
 }
 
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-     x -= 5;
-    
-  } else if (keyCode === RIGHT_ARROW) {
-    
-  }
-}
 
 class Car {
-  constructor(speed, lane, x, y, vx, vy) {
+  constructor(speed, lane, x, y, vx, vy, autowidht, autoheight) {
     this.speed = speed;
     this.lane = lane;
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
-    this.y = 0;
-    this.x = 520;
-    this.autowidth = 
-    this.autohieght = 
+    this.x = 525;
+    this.y = 5; 
+    this.autowidth = 10;
+    this.autoheight = 20;
   }
   draw() {
-    if (this.lane = 3)
-    {
-      this.size = 1;
+    
+    //if (this.lane = 3)
+   // {
+      //this.size = 1;
       image(auto, this.x, this.y);
-      this.vy = 1;
-      this.x = this.x + this.vx;
-      this.y = this.y + this.vy;
-      auto.resize(this.autowidth)
-    }
+     // this.vy = 1;
+     // this.x = this.x + this.vx;
+      //this.y = this.y + this.vy;
+      //auto.resize(this.autowidth)
+  //}
     
     //this.x = this.x + this.vx;
     //this.y = this.y + this.vy;
@@ -65,7 +75,10 @@ function draw() {
   background('blue');
   image(road, 0, 0);
   car1.draw();
-  textSize(20);
+  text("Spookrijder!", 20, 80);
+  text("Ontwijk de naderende auto's!", 20, 60);
   text("Score: " + score, 20, 120);
+  textSize(20);
+  player.draw();
 }
 
