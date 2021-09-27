68d9943fa156d244feeaa038c6c3795cf33888a9
ThijmenStiekema <74553256+ThijmenStiekema@users.noreply.github.com>
2021-09-27 13:47:11 +0000

    Auto rijd nu richting speler

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
-var road, auto, score = 0;
+var road, auto, score;
 
 function preload() {
   road = loadImage('assets/road.png');
-  auto = loadImage('assets/EchteAuto.png');
+  auto = loadImage('assets/NieuwsteAuto2.png');
 }
 
 function setup() {
   createCanvas(1280, 960)
-  car1 = new Car(1, 3, 0, 0, 0, 0);
-  player = new Player(100,'green', 200, 750, 5, -2);
+  car1 = new Car(640, 0, 0, 1, 100, 75);
+  player = new Player(640, 0, 0, 0, 100, 75);
 }
 
 class Player {
-  constructor(radius, kleur, x, y, vx, vy){
-   this.radius = radius;
-   this.kleur = kleur;
+  constructor(x, y, vx, vy, autowidth, autoheight){
    this.x = x;
-   this.x = 600;
-   this.y = 5;
    this.y = y;
    this.vx = vx;
    this.vy = vy;
+   this.autowidth = autowidth;
+   this.autoheight = autoheight;
   }
   draw(){
-  fill('black')
-  rect(this.x,this.y,this.radius,this.radius);
-  if(this.x <= 0 || this.x >= 1280){
-  this.vx = this.vx * -1;
-  }
-  else if(this.y <= 0 || this.y >= 960){
-  this.vy = this.vy * -1;
-  }
+    imageMode(CENTER);
+    image(auto, this.x, this.y, this.autowidth, this.autoheight);
+    this.autowidth = this.autowidth + this.vautowidth;
+    this.autoheight = this.autoheight + this.vautoheight;
   }
 }
 
-
 class Car {
-  constructor(speed, lane, x, y, vx, vy, autowidht, autoheight) {
-    this.speed = speed;
-    this.lane = lane;
+  constructor(x, y, vx, vy, autowidth, autoheight,) {
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
-    this.x = 525;
-    this.y = 5; 
-    this.autowidth = 10;
-    this.autoheight = 20;
+    this.autowidth = autowidth;
+    this.autoheight = autoheight;
   }
   draw() {
+    imageMode(CENTER);
+    image(auto, this.x, this.y, this.autowidth, this.autoheight);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+    this.autowidth = this.autowidth*1.0012;
+    this.autoheight = this.autoheight*1.0012;
+  }
+}
+
+function draw() {
+  background('blue');
+  imageMode(CORNER);
+  image(road, 0, 0);
+  car1.draw();
+  player.draw();
+
+  //text("Spookrijder!", 20, 80);
+  //text("Ontwijk de naderende auto's!", 20, 60);
+  //text("Score: " + score, 20, 120);
+  //textSize(20);
+}
+
     
     //if (this.lane = 3)
    // {
       //this.size = 1;
-      image(auto, this.x, this.y);
-     // this.vy = 1;
-     // this.x = this.x + this.vx;
-      //this.y = this.y + this.vy;
-      //auto.resize(this.autowidth)
+//auto.resize(this.autowidth)
   //}
     
     //this.x = this.x + this.vx;
@@ -68,17 +75,3 @@ class Car {
     //else if (this.y <= 0 || this.y >= 960) {
     //  this.vy = this.vy * -1;
     //}
-  }
-}
-
-function draw() {
-  background('blue');
-  image(road, 0, 0);
-  car1.draw();
-  text("Spookrijder!", 20, 80);
-  text("Ontwijk de naderende auto's!", 20, 60);
-  text("Score: " + score, 20, 120);
-  textSize(20);
-  player.draw();
-}
-
