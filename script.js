6cfa33cb5c0d2d1f03fb0061b1f4ff4ab39c06d3
Mark172004 <74767453+Maku-gif@users.noreply.github.com>
2021-10-01 08:55:37 +0000

    bezig met startscherm

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-var road, auto, score;
-
-function preload() {
-  road = loadImage('assets/road.png');
-  auto = loadImage('assets/NieuwsteAuto2.png');
-  playerauto = loadImage('assets/PlayerAuto.png');
-}
-
-function setup() {
-  createCanvas(1280, 960)
-  car1 = new Car(470, 0, -0.4, 1, 100, 75);
-  car = new Car(640, 0, 0, 1, 100, 75);
-  player = new Player(640, 750, 0, 0, 100, 75);
-}
+var road, auto, score, xcar, ycar, vxcar, vycar;
 
 class Player {
   constructor(x, y, vx, vy, autowidth, autoheight){
@@ -28,52 +15,69 @@ class Player {
     this.autowidth = this.autowidth + this.vautowidth;
     this.autoheight = this.autoheight + this.vautoheight;
     if (keyIsDown(LEFT_ARROW)) {
-    this.x -= 5;
+    this.x -= 10;
     }
     if (keyIsDown(RIGHT_ARROW)) {
-    this.x += 5;
+    this.x += 10;
     }
   }
 }
 
 class Car {
-  constructor(x, y, vx, vy, autowidth, autoheight,) {
-    this.x = x;
-    this.y = y;
-    this.vx = vx;
-    this.vy = vy;
-    this.autowidth = autowidth;
-    this.autoheight = autoheight;
+  constructor(lane, speed){
+    this.x = 430 + lane * 75
+    this.y = 0
+    this.autowidth = 100
+    this.autoheight = 75
+    this.vx = -0.3 + lane*0.15
+    this.vy = 1
   }
   draw() {
     imageMode(CENTER);
     image(auto, this.x, this.y, this.autowidth, this.autoheight);
     this.x = this.x + this.vx;
     this.y = this.y + this.vy;
-    this.autowidth = this.autowidth*1.0012;
-    this.autoheight = this.autoheight*1.0012;
+    if (this.autowidth < 200){
+      this.autowidth = this.autowidth*1.0012;
+      this.autoheight = this.autoheight*1.0012;
+      }
   }
+}
+
+function preload() {
+  road = loadImage('assets/road.png');
+  auto = loadImage('assets/NieuwsteAuto2.png');
+  playerauto = loadImage('assets/PlayerAuto.png');
+  startscherm = loadImage('assets/startschermgame.png');
+}
+
+var cars = [];
+
+function setup() {
+  createCanvas(1280, 960)
+  car = new Car(3,0);
+  cars.push(car);
+  player = new Player(640, 750, 0, 0, 100, 75);
 }
 
 function draw() {
   background('blue');
   imageMode(CORNER);
+  image(startscherm, 0, 0, 1280, 960);
+  if (keyIsDown(ENTER)){gamestart=1};
+  {
   image(road, 0, 0);
-  car1.draw();
-  car.draw();
   
-  
-
-  
+  if(frameCount % 200 == 0){
+     let newCar = new Car(random(0,4));
+     cars.push(newCar);
+    console.log("nieuwe car!");
+  }
+  cars.forEach((c) => {
+    c.draw();
+  });
   player.draw();
-
-
-
-  
-  textAlign(CENTER, BASELINE);
-  text("Score: " + score, 20, 120);
-  text('Spookrijder', 0, 12, width);
-
+  }
 }
 
 //  car = new Car(640, 0, 0, 1, 100, 75);
